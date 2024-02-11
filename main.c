c47f79aaaca8260cce2a8b846c68954aef5fa862
mahziar77 <159702151+mahziar77@users.noreply.github.com>
2024-02-11 08:26:46 -0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main()
+{
+    printf("my name is mahziar!\n");
+    printf("i am learning!\n");
+    printf("programing language!\n");
+    printf("c!\n");
+    return 0;
+}
